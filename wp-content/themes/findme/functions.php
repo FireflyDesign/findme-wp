eed6d4681fa2c1756742436658053531bc9c6d03
FireflyDesign <asor93@o2.pl>
2017-06-06 19:50:37 +0200

    Check if user exists by ajax

--- wp-content/themes/findme/functions.php
+++ wp-content/themes/findme/functions.php
@@ -1,1 +1,23 @@
 <?php
+header('Access-Control-Allow-Headers: X-Requested-With, origin, content-type');
+
+add_action( 'wp_ajax_fm_check_user', 'fm_check_user' );
+add_action( 'wp_ajax_nopriv_fm_check_user', 'fm_check_user' );
+
+function fm_check_user(){
+	
+	$data = $_REQUEST;
+	
+	$username = $_REQUEST['login'];
+	
+	
+    if ( username_exists( $username ) ){		   
+		echo "jest";
+    }else{
+		echo 'nie ma';
+	}
+       
+	  
+	wp_die(); 
+	
+}
